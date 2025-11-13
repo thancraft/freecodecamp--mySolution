704cee9a46e3222ba41803bc48b47d4e5e3618e4
Ahmad Fadli <thancraft313@gmail.com>
2025-11-13 16:25:51 +0900

    Update 13-11-2025.js

--- daily-coding-challage/November/13-11-2025.js
+++ daily-coding-challage/November/13-11-2025.js
@@ -1,11 +1,13 @@
 function generateSignature(name, title, company) {
-  const karakterPertama = name[0].toLowerCase();
-  let hasil = ''
+  const karakterPertama = (name[0] || '').toLowerCase();
+  const output = `${name}, ${title} at ${company}`;
     if (karakterPertama >= 'a' && karakterPertama <= 'i') {
-      return `>>${name}, ${title} at ${company}`;
-    }else if(karakterPertama >= 'j' && karakterPertama <= 'r'){
-      return `--${name}, ${title} at ${company}`;
-    }else if(karakterPertama >= 's' && karakterPertama <= 'z'){
-      return `::${name}, ${title} at ${company}`;
+      return `>>${output}`;
+    }
+    if(karakterPertama >= 'j' && karakterPertama <= 'r'){
+      return `--${output}`;
+    }
+    if(karakterPertama >= 's' && karakterPertama <= 'z'){
+      return `::${output}`;
     }
 }
